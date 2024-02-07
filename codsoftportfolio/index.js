30716cddd5f649b2ca94365749a58480b688afe9
Liza Roy <156820245+Lizaroy04@users.noreply.github.com>
2024-02-07 15:06:39 +0530

    Add files via upload

---
+++ codsoftportfolio/index.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["  C programmer","  Java programmer","  Python programmer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
